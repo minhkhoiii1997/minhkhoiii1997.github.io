511616dc135af36bb8e09327d89b6e7b529b76db
KHOI TRAN <minhkhoiii1997@gmail.com>
2021-05-24 18:19:34 -0600

    deploy

---
+++ precache-manifest.fb165e8c42d74a3b63164b4bda4d4d45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb51109924ee848b982b0430f73d6cc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fac8266ca0de8ab6ca93",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "9f40b02a1668d0eed82f",
+    "url": "/static/js/2.33463950.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.33463950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac8266ca0de8ab6ca93",
+    "url": "/static/js/main.30fc73da.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "3ae6afa3929a7b00aacc33c29be82c7e",
+    "url": "/static/media/highlight-1.3ae6afa3.png"
+  },
+  {
+    "revision": "fd8457b22da8eafeba596df250965af7",
+    "url": "/static/media/highlight-2.fd8457b2.png"
+  },
+  {
+    "revision": "eb38eec24e8fb5add61fc3bcf763fc4b",
+    "url": "/static/media/kevin.eb38eec2.jpeg"
+  },
+  {
+    "revision": "529ccffd29eee2c0e0126c895aa72c1d",
+    "url": "/static/media/resume.529ccffd.pdf"
+  }
+]);
